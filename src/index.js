52ae44f5177f32a604e1bdd6d43613df6845bcb8
Lucas Raposeiras <raposeiras.lucas@gmail.com>
2022-02-23 19:41:52 -0300

    Export validators (#81)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as usePaymentInputs } from './usePaymentInputs';
 export { default as PaymentInputsContainer } from './PaymentInputsContainer';
 export { default as PaymentInputsWrapper } from './PaymentInputsWrapper';
+export { getCardNumberError, getExpiryDateError, getCVCError, getZIPError } from './utils/validator';
